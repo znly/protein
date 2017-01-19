ba4259f98af3e5d0c4b79bf71a1a6167e2cd8181
Clement Rey <cr.rey.clement@gmail.com>
2017-01-19 21:35:58 +0100

    encoder > main interface and default implementation, doc & boilerplate

---
+++ encoder/encoder_versioned.go
@@ -0,0 +1,40 @@
+// Copyright © 2016 Zenly <hello@zen.ly>.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package encoder
+
+import (
+	"github.com/gogo/protobuf/proto"
+	"github.com/znly/tuyauDB/client"
+)
+
+// -----------------------------------------------------------------------------
+
+// EncoderVersioned implements an Encoder that integrates with with znly/tuyauDB
+// in order to add support for versioned protobuf objects via a central registry.
+type EncoderVersioned struct {
+	client client.Client
+}
+
+func New(c client.Client) protein.Encoder {
+}
+
+// -----------------------------------------------------------------------------
+
+func (c *EncoderVersioned) Encode(o proto.Message) ([]byte, error) {
+	return nil, nil
+}
+func (c *EncoderVersioned) EncodeAs(o proto.Message, hash []byte) ([]byte, error) {
+	return nil, nil
+}
