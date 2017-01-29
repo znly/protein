c161bae76e9fbf9108a271f76c8ce1a871bad40e
Clement Rey <cr.rey.clement@gmail.com>
2017-01-29 16:43:50 +0100

    protoscan > reorganized descriptor_tree's deterministic test hashes

--- protoscan/descriptor_tree_test.go
+++ protoscan/descriptor_tree_test.go
@@ -25,6 +25,24 @@ import (
 
 // -----------------------------------------------------------------------------
 
+// `TestSchema` and `DepsEntry` are both immutable, for-testing-purposes
+// only schemas and, as such, both their respective single & recursive
+// hashes can be known in advance and shouldn't ever change unless the
+// `ByteSSlice.Hash` method is ever modified.
+//
+// If such a modification of `ByteSSlice.Hash`'s behavior were to happen,
+// you'd have to modify the following expected values in order to fix the
+// tests.. That is, if you're sure about what you're doing.
+var (
+	psKnownHashSingle = "a9fbbb51c6d1e9aabee6465bba6331cdfad337f420a18d0e3e6f4d8e2cb6035a"
+	deKnownHashSingle = "b0c930d41e1f44abe96a624a1dc9b7b0473c1f5adb8270abc3c67f01244329af"
+
+	psKnownHashRecurse = "bb06a3864164fb5af718c6a2f4f0b70032be9e04a2a756ef45d530d4b6e2a570"
+	deKnownHashRecurse = "d1f1090b178845e096dc0569b7ad93bd0eb711b1fa2d2172a53a7940d5d9024f"
+)
+
+// -----------------------------------------------------------------------------
+
 func _collectTestSchemaTrees(t *testing.T) map[string]*DescriptorTree {
 	// retrieve instanciated gogo/protobuf protofiles
 	symbol := "github.com/gogo/protobuf/proto.protoFiles"
@@ -66,14 +84,9 @@ func TestProtoscan_collectDescriptorTypes(t *testing.T) {
 	b, err := proto.Marshal(psDT.descr)
 	assert.Nil(t, err)
 	assert.NotEmpty(t, b)
-	// `TestSchema` is an immutable, for-testing-purposes only schema, so its
-	// single hash is known in advance & shouldn't ever change unless the
-	// `ByteSSlice.Hash` method is ever modified; in which case you'll have to
-	// modify this test if you're certain about what you're doing
-	psKnownHash := "a9fbbb51c6d1e9aabee6465bba6331cdfad337f420a18d0e3e6f4d8e2cb6035a"
 	psExpectedHash, err := ByteSSlice{b}.Hash()
 	assert.Nil(t, err)
-	assert.Equal(t, psKnownHash, hex.EncodeToString(psExpectedHash))
+	assert.Equal(t, psKnownHashSingle, hex.EncodeToString(psExpectedHash))
 	assert.Equal(t, psExpectedHash, psDT.hashSingle)
 	assert.Nil(t, psDT.hashRecursive)
 
@@ -85,14 +98,9 @@ func TestProtoscan_collectDescriptorTypes(t *testing.T) {
 	b, err = proto.Marshal(deDT.descr)
 	assert.Nil(t, err)
 	assert.NotEmpty(t, b)
-	// `DepsEntry` is an immutable, for-testing-purposes only schema, so its
-	// single hash is known in advance & shouldn't ever change unless the
-	// `ByteSSlice.Hash` method is ever modified; in which case you'll have to
-	// modify this test if you're certain about what you're doing
-	deKnownHash := "b0c930d41e1f44abe96a624a1dc9b7b0473c1f5adb8270abc3c67f01244329af"
 	deExpectedHash, err := ByteSSlice{b}.Hash()
 	assert.Nil(t, err)
-	assert.Equal(t, deKnownHash, hex.EncodeToString(deExpectedHash))
+	assert.Equal(t, deKnownHashSingle, hex.EncodeToString(deExpectedHash))
 	assert.Equal(t, deExpectedHash, deDT.hashSingle)
 	assert.Nil(t, deDT.hashRecursive)
 }
@@ -141,11 +149,6 @@ func TestProtoscan_DescriptorTree_computeRecursiveHash(t *testing.T) {
 	// should only find a dependency to `DepsEntry` in here
 	assert.Len(t, depsMap, 1)
 	assert.Contains(t, depsMap, ".protoscan.TestSchema.DepsEntry")
-	// `TestSchema` is an immutable, for-testing-purposes only schema, so its
-	// recursive hash is known in advance & shouldn't ever change unless the
-	// `ByteSSlice.Hash` method is ever modified; in which case you'll have to
-	// modify this test if you're certain about what you're doing
-	psKnownHash := "bb06a3864164fb5af718c6a2f4f0b70032be9e04a2a756ef45d530d4b6e2a570"
 	// since `TestSchema` has no dependency, its recursive hash should just
 	// be a re-hash of its single hash
 	psExpectedHash, err := ByteSSlice{
@@ -153,20 +156,15 @@ func TestProtoscan_DescriptorTree_computeRecursiveHash(t *testing.T) {
 		depsMap[".protoscan.TestSchema.DepsEntry"].hashSingle,
 	}.Hash()
 	assert.Nil(t, err)
-	assert.Equal(t, psKnownHash, hex.EncodeToString(psExpectedHash))
+	assert.Equal(t, psKnownHashRecurse, hex.EncodeToString(psExpectedHash))
 	assert.Equal(t, psDT.hashRecursive, psExpectedHash)
 
 	deDT := dtsByName[".protoscan.TestSchema.DepsEntry"]
 	assert.Empty(t, deDT.deps) // DepsEntry has no dependency
-	// `DepsEntry` is an immutable, for-testing-purposes only schema, so its
-	// recursive hash is known in advance & shouldn't ever change unless the
-	// `ByteSSlice.Hash` method is ever modified; in which case you'll have to
-	// modify this test if you're certain about what you're doing
-	deKnownHash := "d1f1090b178845e096dc0569b7ad93bd0eb711b1fa2d2172a53a7940d5d9024f"
 	// since `DepsEntry` has no dependency, its recursive hash should just
 	// be a re-hash of its single hash
 	deExpectedHash, err := ByteSSlice{deDT.hashSingle}.Hash()
 	assert.Nil(t, err)
-	assert.Equal(t, deKnownHash, hex.EncodeToString(deExpectedHash))
+	assert.Equal(t, deKnownHashRecurse, hex.EncodeToString(deExpectedHash))
 	assert.Equal(t, deDT.hashRecursive, deExpectedHash)
 }
