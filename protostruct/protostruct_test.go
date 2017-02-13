e2d23281d1127d05167272d247cb3de18ca3b494
Clement Rey <cr.rey.clement@gmail.com>
2017-02-13 11:43:47 +0100

    protostruct > updated tests (matching new tuyaudb APIs)

--- protostruct/protostruct_test.go
+++ protostruct/protostruct_test.go
@@ -40,9 +40,9 @@ func TestProtostruct_CreateStructType(t *testing.T) {
 
 	// build the underlying TuyauDB components: Client{Pipe, KV}
 	bufSize := uint(len(schemas) + 1) // cannot block that way
-	cs, err := tuyau_client.NewSimple(
-		tuyau_pipe.NewRAM(bufSize, bufSize),
-		tuyau_kv.NewRAM(),
+	cs, err := tuyau_client.New(tuyau_client.TYPE_SIMPLE,
+		tuyau_pipe.NewRAMConstructor(bufSize),
+		tuyau_kv.NewRAMConstructor(),
 	)
 	assert.Nil(t, err)
 	assert.NotNil(t, cs)
@@ -51,10 +51,10 @@ func TestProtostruct_CreateStructType(t *testing.T) {
 	// components (i.e. what's pushed into the pipe should en up in the kv
 	// store)
 	ctx, canceller := context.WithCancel(context.Background())
-	s, err := tuyau_service.NewSimple(cs)
+	s, err := tuyau_service.NewSimple(cs, 10)
 	assert.Nil(t, err)
 	assert.NotNil(t, s)
-	go s.Run(ctx, 0)
+	go s.Run(ctx)
 
 	// build the actual Bank that integrates with the TuyauDB Client
 	ty := protein_bank.NewTuyau(cs)
