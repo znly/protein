067d591bd3af73ad3d21237e617ed250882f4d15
Clement Rey <cr.rey.clement@gmail.com>
2017-04-14 00:15:20 +0200

    transcoder:helpers > added TestTranscoder_Helpers_Cassandra

--- transcoder_helpers_test.go
+++ transcoder_helpers_test.go
@@ -18,8 +18,10 @@ import (
 	"context"
 	"reflect"
 	"testing"
+	"time"
 
 	"github.com/garyburd/redigo/redis"
+	"github.com/gocql/gocql"
 	"github.com/rainycape/memcache"
 	"github.com/stretchr/testify/assert"
 	"github.com/znly/protein/protoscan"
@@ -47,8 +49,6 @@ func TestTranscoder_Helpers_Memcached(t *testing.T) {
 
 	testTranscoder_Helpers_common(t, trc)
 }
-
-// -----------------------------------------------------------------------------
 
 func TestTranscoder_Helpers_Redis(t *testing.T) {
 	p := &redis.Pool{
@@ -79,6 +79,57 @@ func TestTranscoder_Helpers_Redis(t *testing.T) {
 	testTranscoder_Helpers_common(t, trc)
 }
 
+func TestTranscoder_Helpers_Cassandra(t *testing.T) {
+	cluster := gocql.NewCluster("localhost:9042")
+	cluster.Consistency = gocql.LocalQuorum
+	cluster.NumConns = 1
+	gocql.TimeoutLimit = 10
+	cluster.ConnectTimeout = time.Second * 10
+	cluster.Timeout = time.Second * 10
+
+	// HACK: Connect to standard 'system' keyspace in order to create
+	//       the keyspace reserved for tuyauDB's blobs.
+	cluster.Keyspace = "system"
+	tmp, err := cluster.CreateSession()
+	assert.Nil(t, err)
+	assert.NotNil(t, tmp)
+
+	// clear the store, just in case
+	keyspaceDropQuery := `DROP KEYSPACE IF EXISTS blobs;`
+	assert.Nil(t, tmp.Query(keyspaceDropQuery).Exec())
+
+	keyspaceCreateQuery := `
+	CREATE KEYSPACE IF NOT EXISTS blobs
+	WITH REPLICATION = {
+		'class':       'NetworkTopologyStrategy',
+		'datacenter1': 1
+	};`
+	err = tmp.Query(keyspaceCreateQuery).Exec()
+	tmp.Close()
+	assert.Nil(t, err)
+
+	cluster.Keyspace = "blobs"
+	s, err := cluster.CreateSession()
+	assert.Nil(t, err)
+	assert.NotNil(t, s)
+	defer s.Close()
+
+	tableCreateQuery := `
+	CREATE TABLE IF NOT EXISTS blobs (key ascii, data blob, PRIMARY KEY (key));`
+	assert.Nil(t, s.Query(tableCreateQuery).Exec())
+
+	// create Transcoder and push all local schemas via user-defined setter
+	trc, err := NewTranscoder(context.Background(),
+		protoscan.SHA256, "PROT-",
+		TranscoderOptGetter(NewTranscoderGetterCassandra(s, "blobs", "key", "data")),
+		TranscoderOptSetter(NewTranscoderSetterCassandra(s, "blobs", "key", "data")),
+	)
+	assert.Nil(t, err)
+	assert.NotNil(t, trc)
+
+	testTranscoder_Helpers_common(t, trc)
+}
+
 // -----------------------------------------------------------------------------
 
 func testTranscoder_Helpers_common(t *testing.T, trc *Transcoder) {
@@ -90,6 +141,7 @@ func testTranscoder_Helpers_common(t *testing.T, trc *Transcoder) {
 	trc.sm = NewSchemaMap()
 	trc.typeCache = map[string]reflect.Type{}
 
+	// decoding will have to use the user-specified getter to find the schemas
 	v, err := trc.Decode(payload)
 	assert.Nil(t, err)
 	assertFieldValues(t, reflect.ValueOf(_transcoderTestSchemaXXX), v)
