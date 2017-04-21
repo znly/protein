c81164e2f2eacf5efbbb0fe6fc9c24acaa7ee6dd
Clement Rey <cr.rey.clement@gmail.com>
2017-04-22 00:14:37 +0200

    ci > ignore coverage for pull-requests

--- coverage.sh
+++ coverage.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
+if [ -z "$COVERALLS_TOKEN" ]; then
+    exit 0 ## ignore coverage for PRs
+fi
+
 echo "mode: set" > coverage.out
 cat *.out | grep -v mode: | sort -r | awk '{if($1 != last) {print $0;last=$1}}' >> coverage.out
+
+
+goveralls -coverprofile=coverage.out -service travis-ci -repotoken "$COVERALLS_TOKEN"
